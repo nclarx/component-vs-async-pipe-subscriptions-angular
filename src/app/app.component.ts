40e414d2a8d5f2da09ae40c22665de78fb0123a7
nclarx <nick@clarx.com.au>
2020-03-05 22:34:49 +1100

    fixed formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy } from "@angular/core";
 import { ObjectService } from "./object.service";
 
-import { Observable, Subscription } from 'rxjs';
+import { Observable, Subscription } from "rxjs";
 
 @Component({
   selector: "my-app",
@@ -12,27 +12,26 @@ export class AppComponent implements OnDestroy {
   public name = "Angular";
   public objects: Array<any>;
   public objectsWithAsyncPipe: Observable<any>;
-  
+
   private objectSubscription: Subscription;
-  
 
   constructor(private objectService: ObjectService) {}
 
-  
-
   ngOnInit() {
     this.getObjects();
-    this.objectsWithAsyncPipe = this.objectService.getAllObjects()
+    this.objectsWithAsyncPipe = this.objectService.getAllObjects();
   }
 
   ngOnDestroy() {
-    this.objectSubscription.unsubscribe()
+    this.objectSubscription.unsubscribe();
   }
 
   getObjects() {
-    this.objectSubscription = this.objectService.getAllObjects().subscribe(data => {
-      console.table(data);
-      this.objects = data;
-    });
+    this.objectSubscription = this.objectService
+      .getAllObjects()
+      .subscribe(data => {
+        console.table(data);
+        this.objects = data;
+      });
   }
 }
